3a0b114b437324a166d52d51035d77eae0c1a395
heavencyj <yc15rice@gmail.com>
2012-10-02 23:22:50 -0500

    initial commit

---
+++ TopPlaces/TopPlaces/TopPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TopPlacesAppDelegate.h
+//  TopPlaces
+//
+//  Created by Heaven Chen on 10/2/12.
+//  Copyright (c) 2012 Heaven Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/TopPlaces/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  TopPlaces
+//
+//  Created by Heaven Chen on 10/2/12.
+//  Copyright (c) 2012 Heaven Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UIViewController
+
+@end
